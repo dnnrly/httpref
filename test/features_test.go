3fd20bae070ed647861ebff487d0eb928eb8e746
Pascal Dennerly <dnnrly@gmail.com>
2021-10-16 22:46:06 +0100

    Fix location of httpref binary

--- test/features_test.go
+++ test/features_test.go
@@ -49,7 +49,7 @@ func theArgsAre(available string) error {
 		Args = strings.Fields(available)
 	}
 
-	cmd := exec.Command("httpref", Args...)
+	cmd := exec.Command("../bin/httpref", Args...)
 	out, err := cmd.Output()
 	if err != nil {
 		var e *exec.ExitError
